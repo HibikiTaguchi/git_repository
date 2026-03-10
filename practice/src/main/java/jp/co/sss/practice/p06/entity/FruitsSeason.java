ab1ba2428d0d7cc35f8ff947e058c6d4b99673e8
h_taguchi <h_taguchi@braingate.co.jp>
2026-03-10 17:13:45 +0900

    03/10退勤時

---
+++ practice/src/main/java/jp/co/sss/practice/p06/entity/FruitsSeason.java
@@ -0,0 +1,41 @@
+package jp.co.sss.practice.p06.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fruits_season")
+public class FruitsSeason {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sfs_gen")
+	@SequenceGenerator(name = "sfs_gen", sequenceName = "seq_fruits_season")
+	private Integer fruitId;
+	@Column
+	private String fruitName;
+	@Column
+	private Integer seasonMonth;
+	
+	public Integer getFruitId() {
+		return fruitId;
+	}
+	public void setFruitId(Integer fruitId) {
+		this.fruitId = fruitId;
+	}
+	public String getFruitName() {
+		return fruitName;
+	}
+	public void setFruitName(String fruitName) {
+		this.fruitName = fruitName;
+	}
+	public Integer getSeasonMonth() {
+		return seasonMonth;
+	}
+	public void setSeasonMonth(Integer seasonMonth) {
+		this.seasonMonth = seasonMonth;
+	}
+}
