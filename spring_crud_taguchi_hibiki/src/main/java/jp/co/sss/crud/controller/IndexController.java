1c9c28f3f90f066ed4c5ca81c097de61d1a1dfc8
h_taguchi <h_taguchi@braingate.co.jp>
2026-03-18 18:00:44 +0900

    03/18退勤時

--- spring_crud_taguchi_hibiki/src/main/java/jp/co/sss/crud/controller/IndexController.java
+++ spring_crud_taguchi_hibiki/src/main/java/jp/co/sss/crud/controller/IndexController.java
@@ -2,11 +2,16 @@ package jp.co.sss.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.crud.entity.Employee;
 import jp.co.sss.crud.form.LoginForm;
 import jp.co.sss.crud.repository.EmployeeRepository;
 
@@ -24,5 +29,34 @@ public class IndexController {
 		session.invalidate();
 		return "index";
 	}
-
+	
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute LoginForm form, BindingResult result, HttpSession session, Model model) {
+		//ログイン判定の前に入力値チェック
+		if (result.hasErrors()) {
+			return "index";
+		}
+		
+		//EmployeeRepositoryのID検索メソッドを使って入力されたIDの従業員情報を取得
+		int id = form.getEmpId();
+		Employee employee = new Employee();
+		employee = employeeRepository.getReferenceById(id);
+		//フォームに入力されたパスワードを取得
+		String inputPass = form.getEmpPass();
+		//入力されたIDに対応する正規のパスワードを取得
+		String truePass = employee.getEmpPass();
+		
+		//入力されたIDとパスワードをDBと照合し整合性を確認
+		if(inputPass.equals(truePass)) {
+			//入力値と登録されたパスワードが一致したのでログイン処理を行う
+			System.out.println("ログイン");
+			//全件表示のための情報を取得
+			model.addAttribute("empList", employeeRepository.findAll());
+			return "list/list";
+		} else {
+			//パスワード不一致のため再度ログイン画面へ
+			System.out.println("ログイン失敗");
+			return "index";
+		}
+	}
 }
--- spring_crud_taguchi_hibiki/src/main/java/jp/co/sss/crud/entity/Employee.java
+++ spring_crud_taguchi_hibiki/src/main/java/jp/co/sss/crud/entity/Employee.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,5 +32,73 @@ public class Employee {
 
 	@Column
 	private Integer authority;
+	
+	@ManyToOne
+	@JoinColumn(name="dept_id", referencedColumnName = "deptId")
+	private Department department;
 
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpPass() {
+		return empPass;
+	}
+
+	public void setEmpPass(String empPass) {
+		this.empPass = empPass;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Integer getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(Integer authority) {
+		this.authority = authority;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
 }
